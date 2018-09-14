5de057e5f18f0567ce3a61a94eeb5e267fc7cdf7
jiangxianzeng <jiangxianzeng@126.com>
2018-09-14 18:36:41 +0800

    add vstream.h and vstream.c

---
+++ src/util/vstream.c
@@ -0,0 +1,20 @@
+/* System library. */
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+/* Utility library. */
+
+#include "mymalloc.h"
+#include "msg.h"
+#include "vbuf_print.h"
+#include "iostuff.h"
+#include "vstring.h"
+#include "vstream.h"
+
+/* Application-specific. */
---
+++ src/util/vstream.h
@@ -0,0 +1,25 @@
+#ifndef _VSTREAM_H_INCLUDED_
+#define _VSTREAM_H_INCLUDED_
+
+/*++
+/* NAME
+/*	vstream 3h
+/* SUMMARY
+/*	simple buffered I/O package
+
+
+ /*
+  * System library.
+  */
+#include <time.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <setjmp.h>
+
+ /*
+  * Utility library.
+  */
+#include <vbuf.h>
+
+
+#endif
